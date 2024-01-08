58908a165ecb97f8e37bfc6abaf95a58e6012c51
rickyli1010 <ricky_0321@hotmail.com>
2024-01-08 13:22:55 -0600

    nav dots and social icons

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './About.scss';
 import { urlFor, client } from '../../client';
@@ -44,4 +45,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
