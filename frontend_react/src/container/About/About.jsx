9383102b15fc3a35e5c8154c4cce924943198bbc
rickyli1010 <ricky_0321@hotmail.com>
2024-01-07 21:41:42 -0600

    add sanity client

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,31 +3,17 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer.',
-    imgUrl: images.about01
-  },
-  {
-    title: 'Frontend Development',
-    description: 'I am a good web developer.',
-    imgUrl: images.about02
-  },
-  {
-    title: 'Backend Development',
-    description: 'I am a good web developer.',
-    imgUrl: images.about03
-  },
-  {
-    title: 'MERN Stack',
-    description: 'I am a good web developer.',
-    imgUrl: images.about04
-  }
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -44,7 +30,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
